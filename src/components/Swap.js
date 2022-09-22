a82cdffbe9819b9ab73541ad4e72872f647d32bb
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-22 14:19:43 +0100

    token images

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -6,18 +6,30 @@ import TokenListModal from "./TokenListModal";
 import "../styles/Swap.css";
 const url = "https://api.pancakeswap.info/api/v2/tokens";
 const Swap = () => {
-  const [tokens, setToken] = useState([]);
+  const [tokens, setTokens] = useState([]);
+  const [currentTokenA, setCurrentTokenA] = useState({});
+  const [currentTokenB, setCurrentTokenB] = useState({});
   const [isListModalToggled, setIsListModalToggled] = useState(false);
+
   const getTokens = () => {
     fetch(url)
       .then((res) => res.json())
       .then((tokens) => {
-        setToken(
-          Object.keys(tokens.data).map(key => {
-            return {token: key,...tokens.data[key]}
-          }).filter((key, index) => {
-            return index < 20;
-          })
+        setTokens(
+          Object.keys(tokens.data)
+            .map((key) => {
+              let currentToken = { address: key, ...tokens.data[key], img:`https://tokens.pancakeswap.finance/images/${key}.png` };
+              if (key === "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c") {
+                setCurrentTokenA(currentToken);
+              }
+              if (key === "0x3203c9E46cA618C8C1cE5dC67e7e9D75f5da2377") {
+                setCurrentTokenB(currentToken);
+              }
+              return { currentToken };
+            })
+            .filter((key, index) => {
+              return index < 20;
+            })
         );
       })
       .catch((error) => {
@@ -29,30 +41,44 @@ const Swap = () => {
   }, [tokens]);
   return (
     <>
-    <div className="swap-container">
-      <div className="swap-box">
-        <div className="swap-title">
-          <h4>Swap</h4>
-          <p className="subtitle">Trade your tokens</p>
-        </div>
-        <div className="swap-body">
-          <div className="form-group">
-            <TokenButton setIsListModalToggled={setIsListModalToggled} isListModalToggled={isListModalToggled}/>
-            <SwapInput />
-            <button className="max-swap-btn">MAX</button>
+      <div className="swap-container">
+        <div className="swap-box">
+          <div className="swap-title">
+            <h4>Swap</h4>
+            <p className="subtitle">Trade your tokens</p>
           </div>
-          <SwitchToken />
-          <div className="form-group">
-            <TokenButton  setIsListModalToggled={setIsListModalToggled} isListModalToggled={isListModalToggled}/>
-            <SwapInput />
-          </div>
-          <div className="form-group">
-            <button className="swap-btn">Swap</button>
+          <div className="swap-body">
+            <div className="form-group">
+              <TokenButton
+                token={currentTokenA}
+                setIsListModalToggled={setIsListModalToggled}
+                isListModalToggled={isListModalToggled}
+              />
+              <SwapInput />
+              <button className="max-swap-btn">MAX</button>
+            </div>
+            <SwitchToken />
+            <div className="form-group">
+              <TokenButton
+                token={currentTokenB}
+                setIsListModalToggled={setIsListModalToggled}
+                isListModalToggled={isListModalToggled}
+              />
+              <SwapInput />
+            </div>
+            <div className="form-group">
+              <button className="swap-btn">Swap</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    {isListModalToggled && (<TokenListModal tokens={tokens} setIsListModalToggled={setIsListModalToggled} isListModalToggled={isListModalToggled}/>)}
+      {isListModalToggled && (
+        <TokenListModal
+          tokens={tokens}
+          setIsListModalToggled={setIsListModalToggled}
+          isListModalToggled={isListModalToggled}
+        />
+      )}
     </>
   );
 };
